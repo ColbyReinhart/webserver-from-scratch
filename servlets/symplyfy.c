8f067cd1a13a55b7b026b9de27d4b49ac499722a
Colby Reinhart <colbyreinhart@gmail.com>
2022-06-20 15:04:32 -0400

    Changed symplyfy servlet to match new website structure

--- servlets/symplyfy.c
+++ servlets/symplyfy.c
@@ -9,7 +9,7 @@ int serve_symplyfy(int sock_fd, char* requested_file)
 {
 	char file_path[200];
 	strcpy(file_path, ROOT_PATH);
-	strcat(file_path, "/symplyfy/");
+	strcat(file_path, "/website/symplyfy/");
 	strcat(file_path, requested_file);
 	return send_normal_response(sock_fd, http_200, file_path);
 }
