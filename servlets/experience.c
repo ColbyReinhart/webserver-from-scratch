1c6476ce8ecf5d5b34d07d3dac1c4df6c1beee41
Colby Reinhart <colbyreinhart@gmail.com>
2022-07-10 13:48:13 -0400

    Fixed experience page routing error.

--- servlets/experience.c
+++ servlets/experience.c
@@ -9,7 +9,7 @@ int serve_experience(int sock_fd, char* requested_file)
 {
 	char file_path[200];
 	strcpy(file_path, ROOT_PATH);
-	strcat(file_path, "/website/aboutme/");
+	strcat(file_path, "/website/experience/");
 	strcat(file_path, requested_file);
 	return send_normal_response(sock_fd, http_200, file_path);
 }
