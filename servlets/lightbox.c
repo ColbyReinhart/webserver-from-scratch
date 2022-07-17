ca80522465d8b6d562bb12ba315764be7c2e377e
Colby Reinhart <colbyreinhart@gmail.com>
2022-07-17 17:21:36 -0400

    Cleanup

--- servlets/lightbox.c
+++ servlets/lightbox.c
@@ -9,33 +9,25 @@ extern int lightbox_socket;
 
 int serve_lightbox(int sock_fd, char* requested_file)
 {
-	printf("Lightbox servlet called\n");///
 	if (strcmp(requested_file, "connect.action") == 0)
 	{
-		const char* response = "r=255 g=255 b=255\r\n";
-		const int code = write(lightbox_socket, response, strlen(response));
-		printf("Code: %i\n", code);
-		if (code == -1)
-		{
-			perror("What happened: ");
-		}
 		return 100;
 	}
 	else if (strcmp(requested_file, "red.action") == 0)
 	{
-		const char* response = "r=255 g=0 b=0\r\n";
+		const char* response = "255 0 0\r\n";
 		write(lightbox_socket, response, strlen(response));
 		return send_empty_response(sock_fd, http_200);
 	}
 	else if (strcmp(requested_file, "green.action") == 0)
 	{
-		const char* response = "r=0 g=255 b=0\r\n";
+		const char* response = "0 255 0\r\n";
 		write(lightbox_socket, response, strlen(response));
 		return send_empty_response(sock_fd, http_200);
 	}
 	else if (strcmp(requested_file, "blue.action") == 0)
 	{
-		const char* response = "r=0 g=0 b=255\r\n";
+		const char* response = "0 0 255\r\n";
 		write(lightbox_socket, response, strlen(response));
 		return send_empty_response(sock_fd, http_200);
 	}
